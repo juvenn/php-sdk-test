69f319b9f00769363719b37ebfd4758d5a18b859
Juvenn Woo <machese@gmail.com>
2017-06-15 15:09:00 +0800

    Fix get env variables

--- public/test/env.php
+++ public/test/env.php
@@ -2,15 +2,16 @@
 <html>
   <head>
     <title>LeanEngine PHP</title>
+    <meta charset="UTF-8">
     <link rel="stylesheet" href="/stylesheets/style.css">
   </head>
   <body>
     <h2>LeanEngine PHP 环境变量</h2>
     <dl>
-      <dd><code>PHP_SELF=<?php echo getenv("PHP_SELF"); ?></code></dd>
-      <dd><code>PATH_INFO=<?php echo getenv("PATH_INFO"); ?></code></dd>
-      <dd><code>QUERY_STRING=<?php echo getenv("QUERY_STRING"); ?></code></dd>
-      <dd><code>X_REAL_IP=<?php echo getenv("X_REAL_IP"); ?></code></dd>
+      <dd><code>PHP_SELF=<?php echo $_SERVER["PHP_SELF"]; ?></code></dd>
+      <dd><code>PATH_INFO=<?php echo $_SERVER["PATH_INFO"]; ?></code></dd>
+      <dd><code>QUERY_STRING=<?php echo $_SERVER["QUERY_STRING"]; ?></code></dd>
+      <dd><code>X_REAL_IP=<?php echo $_SERVER["X_REAL_IP"]; ?></code></dd>
     </dl>
     <p>请参考 <a href="https://leancloud.cn/docs/leanstorage_guide-php.html">PHP 数据存储开发指南</a></p>
   </body>
