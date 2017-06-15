b935a136f6c191e18dc592567dd02e17575dcb0c
Juvenn Woo <machese@gmail.com>
2017-06-15 16:14:20 +0800

    Fix env vars

--- public/test/env.php
+++ public/test/env.php
@@ -11,7 +11,7 @@
       <dd><code>PHP_SELF=<?php echo $_SERVER["PHP_SELF"]; ?></code></dd>
       <dd><code>PATH_INFO=<?php echo $_SERVER["PATH_INFO"]; ?></code></dd>
       <dd><code>QUERY_STRING=<?php echo $_SERVER["QUERY_STRING"]; ?></code></dd>
-      <dd><code>X_REAL_IP=<?php echo $_SERVER["X_REAL_IP"]; ?></code></dd>
+      <dd><code>HTTP_X_REAL_IP=<?php echo $_SERVER["HTTP_X_REAL_IP"]; ?></code></dd>
     </dl>
     <p>请参考 <a href="https://leancloud.cn/docs/leanstorage_guide-php.html">PHP 数据存储开发指南</a></p>
   </body>
